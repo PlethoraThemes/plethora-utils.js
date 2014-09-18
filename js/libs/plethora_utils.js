8315f5c9c3ba8357c04e62276775d83c37a2107b
Kostas X <kostas@creativecollective.gr>
2014-09-18 03:47:48 +0300

    Added tests for TRUE,FALSE,True,False strings

--- js/libs/plethora_utils.js
+++ js/libs/plethora_utils.js
@@ -1,8 +1,9 @@
 var plethoraUtils = plethoraUtils || {};
 
 /*
- * Convert values: 1, 0, true, false, "1", "0", "true", "false" to booleans
+ * Convert values: 1, 0, true, false, "1", "0", "true", "false", "TRUE", "FALSE", "True", "False" to booleans
  */
  plethoraUtils.checkBool = function(val){
-	return ( ""+val === "1" || ""+val === "true" ) ? true : false;
+ 	val = val.toString().toLowerCase();
+	return ( val === "1" || val === "true" ) ? true : false;
 };
--- js/tests/tests.js
+++ js/tests/tests.js
@@ -29,6 +29,22 @@ QUnit.test( "Test equality with checkBool()", function( assert ) {
 	result = plethoraUtils.checkBool(value);
 	assert.deepEqual( result, false, "Testing with value: " + value + " of type: " + typeof value);
 
+	value  = "TRUE";
+	result = plethoraUtils.checkBool(value);
+	assert.deepEqual( result, true, "Testing with value: " + value + " of type: " + typeof value);
+
+	value  = "FALSE";
+	result = plethoraUtils.checkBool(value);
+	assert.deepEqual( result, false, "Testing with value: " + value + " of type: " + typeof value);
+
+	value  = "True";
+	result = plethoraUtils.checkBool(value);
+	assert.deepEqual( result, true, "Testing with value: " + value + " of type: " + typeof value);
+
+	value  = "False";
+	result = plethoraUtils.checkBool(value);
+	assert.deepEqual( result, false, "Testing with value: " + value + " of type: " + typeof value);
+
 	value  = true;
 	result = plethoraUtils.checkBool(value);
 	assert.deepEqual( result, true, "Testing with value: " + value + " of type: " + typeof value);
