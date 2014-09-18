577b7a865e70accf194041a4bf2ad9f8b34eb707
Kostas X <kostas@creativecollective.gr>
2014-09-18 03:34:17 +0300

    Changed assertions to deepEqual()

--- js/tests/tests.js
+++ js/tests/tests.js
@@ -7,34 +7,34 @@ QUnit.test( "Test equality with checkBool()", function( assert ) {
 
 	var value  = 1;
 	var result = plethoraUtils.checkBool(value);
-	assert.ok( result, "Testing with value: " + value + " of type: " + typeof value);
+	assert.deepEqual( result, true, "Testing with value: " + value + " of type: " + typeof value);
 
 	value  = 0;
 	result = plethoraUtils.checkBool(value);
-	assert.ok( !result, "Testing with value: " + value + " of type: " + typeof value);
+	assert.deepEqual( result, false, "Testing with value: " + value + " of type: " + typeof value);
 
 	value  = "1";
 	result = plethoraUtils.checkBool(value);
-	assert.ok( result, "Testing with value: " + value + " of type: " + typeof value);
+	assert.deepEqual( result, true, "Testing with value: " + value + " of type: " + typeof value);
 
 	value  = "0";
 	result = plethoraUtils.checkBool(value);
-	assert.ok( !result, "Testing with value: " + value + " of type: " + typeof value);
+	assert.deepEqual( result, false, "Testing with value: " + value + " of type: " + typeof value);
 
 	value  = "true";
 	result = plethoraUtils.checkBool(value);
-	assert.ok( result, "Testing with value: " + value + " of type: " + typeof value);
+	assert.deepEqual( result, true, "Testing with value: " + value + " of type: " + typeof value);
 
 	value  = "false";
 	result = plethoraUtils.checkBool(value);
-	assert.ok( !result, "Testing with value: " + value + " of type: " + typeof value);
+	assert.deepEqual( result, false, "Testing with value: " + value + " of type: " + typeof value);
 
 	value  = true;
 	result = plethoraUtils.checkBool(value);
-	assert.ok( result, "Testing with value: " + value + " of type: " + typeof value);
+	assert.deepEqual( result, true, "Testing with value: " + value + " of type: " + typeof value);
 
 	value  = false;
 	result = plethoraUtils.checkBool(value);
-	assert.ok( !result, "Testing with value: " + value + " of type: " + typeof value);
+	assert.deepEqual( result, false, "Testing with value: " + value + " of type: " + typeof value);
 
 });
