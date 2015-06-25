70e76332688c187b4e80960761622851d65851d2
Kostas Minaidis <kostas@creativecollective.gr>
2015-06-25 14:17:17 +0300

    Updating library of helper functions

--- js/libs/plethora_utils.js
+++ js/libs/plethora_utils.js
@@ -1,4 +1,4 @@
-var plethoraUtils = plethoraUtils || {};
+var _p = _p || {};
 
 /*
  * Convert values: 1, 0, true, false, "1", "0", "true", "false", "TRUE", "FALSE", "True", "False"
@@ -6,13 +6,77 @@ var plethoraUtils = plethoraUtils || {};
  */
 
 /* READABLE VERSION 
-plethoraUtils.checkBool = function(val){
+_p.checkBool = function(val){
 	// Check if numeric value is greater than 0
 	val = (typeof val !=="number") ? val : (val>0);
 	return ({1:1,true:1,on:1,yes:1}[(val+"").toLowerCase()]) ? true : false;
 };
 */
 
-plethoraUtils.checkBool = function(val){
+_p.checkBool = function(val){
 	return ({1:1,true:1,on:1,yes:1}[(((typeof val !=="number")?val:(val>0))+"").toLowerCase()])?true:false;
 };
+
+/*** OBJECT EXTEND: By @toddmotto ***/
+
+_p.extend = function( target, source ) {
+  var merged = Object.create(target);
+  Object.keys(source).map(function (prop) {  prop in merged && (merged[prop] = source[prop]);  });
+  return merged;
+};
+
+/*
+_p.extend = function( target, source ) {
+for( var key in source ) {  if( source.hasOwnProperty( key ) ) target[key] = source[key];  }
+return target;
+}
+*/
+
+/*** MULTI SLICE ***/
+
+_p.slice = function(){
+	return [].slice.call.apply( [].slice, arguments );
+}
+
+/*** DEBUGGING CONSOLE ***/
+
+_p.debugLog = function(){
+	themeConfig && themeConfig.debug && console.log.apply( console, arguments );
+}
+
+/*** SVG CREATION UTILITY FUNCTION ***/
+
+_p.SVGMold  = function( type, options ){
+	var molding = doc.createElementNS('http://www.w3.org/2000/svg', type );
+	for (var key in options) options.hasOwnProperty(key) && molding.setAttribute( key, options[key]);
+	return molding;
+}
+
+/*** COLORED CONSOLE LOG ***/
+
+_p.clog = function( msg ){
+
+  // TODO: CHECK IF IS CHROME WHERE CONSOLE CSS IS SUPPORTED: ( window.chrome )
+
+  var tag           = "clog";
+  var args          = [].slice.apply(arguments);
+  var currentScript = "";
+
+  if ( document.currentScript ){
+    currentScript = document.currentScript.src.split('/');
+    currentScript = currentScript[currentScript.length-1].split("?")[0];
+    tag = currentScript;
+  }
+
+  args.unshift(
+    '%c [ %s ]: %c',
+    'background: #222; color: #bada55',
+    tag,
+    'background: #222; color: #2ada45'
+  );
+
+  console.log.apply( console, args );
+
+}
+
+// USAGE: _p.clog( "Debugging this object", myObject );
--- js/tests/tests.js
+++ js/tests/tests.js
@@ -7,143 +7,143 @@ QUnit.test( "Test equality with checkBool()", function( assert ) {
 
 	var value    = 1;
 	var expected = true;
-	var result   = plethoraUtils.checkBool(value);
+	var result   = _p.checkBool(value);
 
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = 0;
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "1";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "0";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "true";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "false";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "TRUE";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "FALSE";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "True";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "False";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = true;
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = false;
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "On";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "Off";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "ON";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "OFF";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "on";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "off";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "YES";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "NO";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "Yes";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "No";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "yes";
 	expected = true;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "no";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = undefined;
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = null;
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "undefined";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 	value    = "randomstring";
 	expected = false;
-	result   = plethoraUtils.checkBool(value);
+	result   = _p.checkBool(value);
 	assert.deepEqual( result, expected, "Testing with value: " + value + " of type: " + typeof value + " which should evaluate to " + expected);
 
 });
